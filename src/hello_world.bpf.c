6b5c1868f772b81249c9c5b2e4c5cc2c92c4eb30
Seyed Javad Hosseini <javadh75@gmail.com>
2021-07-08 17:25:04 +0430

    Delete extra lines

--- src/hello_world.bpf.c
+++ src/hello_world.bpf.c
@@ -9,23 +9,3 @@ int bpf_hello_world(void *ctx) {
 }
 
 char LICENSE[] SEC("license") = "GPL";
-// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
-/* Copyright (c) 2020 Facebook */
-// #include <bpf/bpf_helpers.h>
-// #include <linux/bpf.h>
-
-// char LICENSE[] SEC("license") = "Dual BSD/GPL";
-
-// int my_pid = 0;
-
-// SEC("tp/syscalls/sys_enter_write")
-// int handle_tp(void *ctx) {
-//   int pid = bpf_get_current_pid_tgid() >> 32;
-
-//   if (pid != my_pid)
-//     return 0;
-
-//   bpf_printk("BPF triggered from PID %d.\n", pid);
-
-//   return 0;
-// }
